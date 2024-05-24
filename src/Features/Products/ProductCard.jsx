50772d59c105f1e559de7bb8134dba708dce69c2
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-05-24 16:44:23 +0330

    feat: develop category sidebar

--- src/Features/Products/ProductCard.jsx
+++ src/Features/Products/ProductCard.jsx
@@ -1,44 +1,63 @@
+"use client";
+import { Chip } from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProductCard = ({slug , imageLink , title , price , countInStock}) => {
-    return ( 
-        <div className="flex flex-col items-center gap-y-5">
-        <Link href={`products/${slug}`} className="min-h-[297px]">
-          <Image
-            width={222}
-            height={297}
-            alt="ghorbani-dev.ir"
-            placeholder="blur"
-            blurDataURL={`/images/products/${imageLink}`}
-            src={`/images/products/${imageLink}`}
-          />
-        </Link>
-        <Link
-          href={`products/${slug}`}
-          className="text-center font-semibold leading-7 line-clamp-2 min-h-16"
-        >
-          {title}
-        </Link>
-        <div className="flex-center gap-1.5">
-            {
-                    countInStock > 0 ? <>
-                <span className="text-lg font-semibold">
-                  {price.toLocaleString()}
-                </span>
-                <Image
-                  width={25}
-                  height={25}
-                  alt="ghorbani-dev.ir"
-                  placeholder="blur"
-                  blurDataURL="/images/toman/toman.svg"
-                  src="/images/toman/toman.svg"
-                  className="rounded-none"
-                /></> : <span className="text-rose-500">اتمام موجودی</span>
-            }
-        </div>
-      </div>
-     );
-}
- 
+const ProductCard = ({ product }) => {
+  const { slug, imageLink, title, price, countInStock, discount, offPrice } =
+    product;
+  return (
+    <div className="flex flex-col gap-y-5">
+      <Link href={`products/${slug}`} className="min-h-[297px]">
+        <Image
+          width={222}
+          height={297}
+          alt="ghorbani-dev.ir"
+          placeholder="blur"
+          blurDataURL={`/images/products/${imageLink}`}
+          src={`/images/products/${imageLink}`}
+        />
+      </Link>
+      <Link
+        href={`products/${slug}`}
+        className="text-center font-semibold leading-7 line-clamp-2 min-h-16"
+      >
+        {title}
+      </Link>
+      {countInStock > 0 ? (
+        <>
+          <div className={`${discount > 0 ? "flex-between" : "flex-center"}`}>
+            {discount > 0 && (
+              <>
+                <p className="text-gray-400 line-through">
+                  {offPrice.toLocaleString()}
+                </p>
+                <Chip color="secondary" variant="solid">
+                  %{discount}
+                </Chip>
+              </>
+            )}
+            <div className="flex-center gap-1">
+              <span className="text-lg font-semibold">
+                {price.toLocaleString()}
+              </span>
+              <Image
+                width={20}
+                height={20}
+                alt="ghorbani-dev.ir"
+                placeholder="blur"
+                blurDataURL="/images/toman/toman.svg"
+                src="/images/toman/toman.svg"
+                className="rounded-none"
+              />
+            </div>
+          </div>
+        </>
+      ) : (
+        <p className="text-rose-500 flex-center">اتمام موجودی</p>
+      )}
+    </div>
+  );
+};
+
 export default ProductCard;
--- src/app/(users)/products/page.jsx
+++ src/app/(users)/products/page.jsx
@@ -2,19 +2,20 @@ import ProductCard from "@/Features/Products/ProductCard";
 import { GetCategories } from "@/Services/CategoryService";
 import { GetProducts } from "@/Services/ProductServices";
 import React from "react";
+import CategorySidebar from "./[slug]/CategorySidebar";
 
 const Products = async () => {
   const { products } = await GetProducts();
   const { categories } = await GetCategories();
   return (
     <section className="container grid items-start grid-rows-1 grid-cols-4 gap-3.5 sm:gap-5 mt-9 sm:mt-25 my-16">
-      <div className="hidden md:grid md:col-span-1">1</div>
+      <div className="hidden md:grid md:col-span-1"><CategorySidebar categories={categories}/></div>
       <div className="col-span-4 md:col-span-3">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          {products.map(({ _id, imageLink, title, slug, price , countInStock}) => {
+          {products.map((product) => {
             return (
-              <React.Fragment key={_id}>
-               <ProductCard imageLink={imageLink} title={title} slug={slug} price={price} countInStock={countInStock}/>
+              <React.Fragment key={product._id}>
+               <ProductCard product={product}/>
               </React.Fragment>
             );
           })}
