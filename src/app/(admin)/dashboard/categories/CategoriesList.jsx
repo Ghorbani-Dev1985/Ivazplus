6def48307c252cd67db5217f5253d0b70b3ce857
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-06-01 17:51:07 +0330

    feat: develop remove category

--- src/app/(admin)/dashboard/categories/CategoriesList.jsx
+++ src/app/(admin)/dashboard/categories/CategoriesList.jsx
@@ -1,12 +1,29 @@
 import { categoryListTableTHeads} from "@/Constants/TableHeaders";
+import ConfirmModal from "@/UI/ConfimModal";
 import CustomTable from "@/UI/CustomTable";
-import { Chip } from "@nextui-org/react";
+import { Button, Chip } from "@nextui-org/react";
+import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { useCallback } from "react";
+import toast from "react-hot-toast";
 import { BiEditAlt, BiTrash } from "react-icons/bi";
+import { useRemoveCategory } from "src/hooks/useCategories";
 import ToLocalDateStringShort from "src/utils/ToLocalDateStringShort";
 
 const CategoriesList = ({categoryArrayItem}) => {
+  const {mutateAsync} = useRemoveCategory();
+  const queryClient = useQueryClient();
+  const RemoveCategoryHandler = async (id) => {
+    try {
+      const { message } = await mutateAsync(id);
+      toast.success(message);
+      queryClient.invalidateQueries({ queryKey: ["getCategories"] });
+    } catch (error) {
+      if (error?.response?.data) {
+        toast.error(error.response.data.message);
+      }
+    }
+  }
   const renderCategoriesCell = useCallback((category, columnKey) => {
     const cellValue = category[columnKey];
         switch (columnKey) {
@@ -22,8 +39,16 @@ const CategoriesList = ({categoryArrayItem}) => {
             return category.description;
             case "act":
               return <div className="flex-between gap-x-1.5">
-                 <button><Link href={`/dashboard/categories/edit/${category._id}`}><BiEditAlt className="size-6 text-sky-500" /></Link></button>
-                 <button><BiTrash className="size-6 text-rose-500" /></button>
+                 <Button isIconOnly color="default" variant="light">
+              <Link href={`/dashboard/categories/edit/${category._id}`}>
+                <BiEditAlt className="size-6 text-sky-500" />
+              </Link>
+            </Button>
+            <ConfirmModal
+              btnIcon={<BiTrash className="size-6 text-rose-500" />} titleText="حذف دسته بندی" confirmBtnText="حذف دسته بندی" confirmBtnHandler={() => RemoveCategoryHandler(category._id)}
+            >
+              آیا برای حذف دسته بندی <span className="text-rose-400">{category.title}</span> مطمعن هستید؟
+            </ConfirmModal>
               </div>
           default:
             return cellValue;
